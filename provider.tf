15a289f54a3d855941e3c22053cd14f2dde0bce5
bandanacodes <bandana0477@gmail.com>
2025-09-04 10:54:13 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-0a232144cf20a27a5"
+instance_type = "t2.medium"
+key_name = "keybandana"
+vpc_security_group_ids = ["sg-05170795575d8e334"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "APPSERVER-1", "APPSERVER-2", "Monitoring server"]
+}
